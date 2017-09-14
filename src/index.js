ca7127643a9cb503ed03af5424d22e7d0f3db57a
Gabriel <gabrieldarezzo@yahoo.com.br>
2017-09-13 21:41:57 -0300

    andamento

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { API_URL, TOKEN_API } from './config';
+
+
 import {
   search,
   searchArtists,
@@ -13,14 +16,26 @@ import {
   getAlbumTracks,
 } from './album';
 
-module.exports = {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchTracks,
-  searchPlaylists,
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-};
+// module.exports = {
+//   search,
+//   searchArtists,
+//   searchAlbums,
+//   searchTracks,
+//   searchPlaylists,
+//   getAlbum,
+//   getAlbums,
+//   getAlbumTracks,
+// };
 
+export class SpotifyWrapper {
+  constructor(options) {
+    this.apiURL = options.apiURL || API_URL;
+    this.token = options.token || TOKEN_API;
+  }
+
+  request(url) {
+    return fetch(url);
+
+  }
+
+}
---
+++ tests/index.spec.js
@@ -0,0 +1,63 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
+
+import { SpotifyWrapper } from '../src/index';
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+global.fetch = require('node-fetch');
+
+describe('SpotifyWrapper Library', () => {
+  it('should create an instance of SpotifyWrapper', () => {
+    const spotify = new SpotifyWrapper({});
+    expect(spotify).to.be.an.instanceof(SpotifyWrapper);
+  });
+
+  it('should receive apiURL as an option', () => {
+    const spotify = new SpotifyWrapper({
+      apiURL: 'blablabla',
+    });
+
+    expect(spotify.apiURL).to.be.equal('blablabla');
+  });
+
+
+  it('should receive apiURL if not provided', () => {
+    const spotify = new SpotifyWrapper({});
+    expect(spotify.apiURL).to.be.equal('https://api.spotify.com/v1');
+  });
+
+
+  it('should receive token as an option', () => {
+    const spotify = new SpotifyWrapper({
+      token: 'foo',
+    });
+    expect(spotify.token).to.be.equal('foo');
+  });
+});
+
+
+describe('request method', () => {
+
+  let fetchStub;
+  let promise;
+
+  beforeEach(() => {
+    fetchStub = sinon.stub(global, 'fetch');
+    promise = fetchStub.returnsPromise();
+  });
+
+  afterEach(() => {
+    fetchStub.restore();
+  });
+
+
+  it('should have request method', () => {
+    const spotify = new SpotifyWrapper({});
+    expect(spotify.request).to.exist;
+  });
+
+});
